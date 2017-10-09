47cc86c584a25b5ae5a75a1749d86af8ee42d720
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-09 19:28:29 +0100

    Reseted Author to default Value.

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,22 +1,17 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 5/16/17.
  */
-@Entity
+
 public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String firstName;
     private String lastName;
 
-    @ManyToMany(mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
 
     public Author() {
@@ -31,14 +26,6 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
