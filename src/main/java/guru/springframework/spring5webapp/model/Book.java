00dec46093d9ca5f80c39184284ecdbec88a18de
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-09 19:29:55 +0100

    Reset Book to default Value.

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,24 +1,16 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 5/16/17.
  */
-@Entity
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
-    @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
@@ -35,14 +27,6 @@ public class Book {
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getTitle() {
